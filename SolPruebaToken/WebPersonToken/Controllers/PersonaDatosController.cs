03747c2b9ef113a0325e142bce238ae371c0adce
CristhianSoto <a2018210980@estudiantes.upsa.edu.bo>
2021-04-23 22:53:21 -0400

    se agrego el controlador MVC PeopleController

---
+++ SolPruebaToken/WebPersonToken/Controllers/PersonaDatosController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebPersonToken.Models;
+
+namespace WebPersonToken.Controllers
+{
+    public class PersonaDatosController : Controller
+    {
+        private DataContext db = new DataContext();
+
+        // GET: PersonaDatos
+        public ActionResult Index()
+        {
+            return View(db.PersonaDatos.ToList());
+        }
+
+        // GET: PersonaDatos/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            PersonaDatos personaDatos = db.PersonaDatos.Find(id);
+            if (personaDatos == null)
+            {
+                return HttpNotFound();
+            }
+            return View(personaDatos);
+        }
+
+        // GET: PersonaDatos/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: PersonaDatos/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "PersonId,Name,CovidCount")] PersonaDatos personaDatos)
+        {
+            if (ModelState.IsValid)
+            {
+                db.PersonaDatos.Add(personaDatos);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(personaDatos);
+        }
+
+        // GET: PersonaDatos/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            PersonaDatos personaDatos = db.PersonaDatos.Find(id);
+            if (personaDatos == null)
+            {
+                return HttpNotFound();
+            }
+            return View(personaDatos);
+        }
+
+        // POST: PersonaDatos/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "PersonId,Name,CovidCount")] PersonaDatos personaDatos)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(personaDatos).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(personaDatos);
+        }
+
+        // GET: PersonaDatos/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            PersonaDatos personaDatos = db.PersonaDatos.Find(id);
+            if (personaDatos == null)
+            {
+                return HttpNotFound();
+            }
+            return View(personaDatos);
+        }
+
+        // POST: PersonaDatos/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            PersonaDatos personaDatos = db.PersonaDatos.Find(id);
+            db.PersonaDatos.Remove(personaDatos);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
